ad050332d3a042ea3ed14a1da8ca974cbd0f2aa7
Aakrit Subedi <aakritsubedi@pop-os.localdomain>
2021-04-24 10:23:13 +0545

    SERVER: Add socket in server and integrate with text-editor

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const io = require('socket.io')(3001, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST']
+  }
+});
+
+io.on("connection", socket => {
+  socket.on('send-text-changes', delta => {
+    socket.broadcast.emit('receive-text-changes', delta);
+  });
+});
