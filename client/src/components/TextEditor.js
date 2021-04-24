32402b2d7cee6728e9ef377df2d227070ac75a07
Aakrit Subedi <aakritsubedi@pop-os.localdomain>
2021-04-24 10:22:46 +0545

    SERVER: Add socket in server and integrate with text-editor

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 import Quill from 'quill';
 
@@ -11,21 +12,66 @@ const TOOLBAR_OPTIONS = [
   ['bold', 'italic', 'underline'],
   [{ color: [] }, { background: [] }],
   [{ scripts: 'sub' }, { scripts: 'super' }],
-  [{ 'indent': '-1'}, { 'indent': '+1' }],
-  [{ size: [ 'small', false, 'large', 'huge' ]}],
+  [{ indent: '-1' }, { indent: '+1' }],
+  [{ size: ['small', false, 'large', 'huge'] }],
   [{ align: [] }],
   ['image', 'blockquote', 'code-block'],
   ['clean'],
 ];
 
 const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    console.log('Connecting ....');
+    const s = io('http://localhost:3001');
+    setSocket(s);
+
+    return () => {
+      console.log('Dis-connecting ....');
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== 'user') return;
+      socket.emit('send-text-changes', delta);
+    };
+    quill.on('text-change', handler);
+
+    return () => {
+      quill.off('text-change', handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    console.log(socket);
+    
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+
+    socket.on('receive-text-changes', handler);
+
+    return () => {
+      socket.off('receive-text-changes', handler);
+    };
+  }, [socket, quill]);
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
 
     wrapper.innerHTML = '';
     const editor = document.createElement('div');
     wrapper.append(editor);
-    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+    const q = new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+
+    setQuill(q);
   }, []);
 
   return <div className="editor" ref={wrapperRef}></div>;
